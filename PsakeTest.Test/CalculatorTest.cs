e0d6b5bcee233f2d5f768f7e478b80d2a44e36a9
Jeroen Bok <jeroenbok@gmail.com>
2012-04-20 19:02:10 +0200

    added initial test project, with fantastic Calculator class

---
+++ PsakeTest.Test/CalculatorTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using NUnit.Framework;
+
+namespace PsakeTest.Test
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void Sum()
+        {
+            Assert.That(new Calculator().Sum(1, 1), Is.EqualTo(2), "sum");
+        }
+
+        [Test]
+        [Category("Acceptance")]
+        public void SumAcceptance()
+        {
+            Thread.Sleep(2000);
+            Assert.That(new Calculator().Sum(10, 10), Is.EqualTo(20), "sum");
+        }
+    }
+}
---
+++ PsakeTest/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PsakeTest
+{
+    public class Calculator
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
